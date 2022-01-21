bf5b75771096c218cf70c6588733478bd54e8c67
JuanManuelGalvan <98127350+JuanManuelGalvan@users.noreply.github.com>
2022-01-21 13:58:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+'use stric' //es para utilizar un modo estricto en JS
+
+//cargar el modulo de mongoose para conectar con la base de datos de mongo
+var mongoose=require('mongoose');//el primer 'mongoose' es la variable
